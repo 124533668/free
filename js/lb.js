93d8fb6e8b11bb569f17ae807bbbcbe7d13fefd1
124533668 <124533668@qq.com>
2017-06-06 15:58:46 +0800

    xinsousuo

---
+++ js/lb.js
@@ -0,0 +1,31 @@
+ var mySwiper = new Swiper ('#lba', {
+    loop: true,
+    pagination: '.swiper-pagination',
+    });
+
+ var zthd = new Swiper ('#lbb', {
+     resistanceRatio : 0,
+ })
+
+
+
+// 搜索框
+ $('#ssk').on('input',function () {
+     var str = $(this).val();
+     $.ajax({
+         type:'GET',
+         url:"http://api.dagoogle.cn/music/music-and-lrc",
+         data:{count:8,keyword:str},
+         dataType:'jsonp',
+         jsonp:'callback',
+         jsonpCallback:'fn',
+         success:function (res) {
+             let data = res.data;
+             let txt = "";
+             $.each(data,function (index,itme) {
+                 txt += `<li><aside><p>${itme.filename}</p></aside></li>`;
+                 $('.recUl').html(txt)
+             })
+         }
+     })
+ })
